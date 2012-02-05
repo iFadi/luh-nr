15707c4fdc998b92ba4482de757f7b7c7a8ad3b2
iFadi <fadi_asbih@yahoo.de>
2012-02-05 00:21:01 +0000

    Code Refactoring, Update Notifier is added.

--- src/controller/RunApp.java
+++ src/controller/RunApp.java
@@ -1,13 +1,50 @@
 package controller;
 
 import model.ReadPDF;
+import model.UpdateNotifier;
+import view.UpdateView;
 import view.View;
-
+/**
+ * 
+ * RunApp.java
+ * 
+ * 
+ * @author Fadi Asbih
+ * @email fadi_asbih@yahoo.de
+ * @version 1.1.0  04/02/2012
+ * @copyright 2012
+ * 
+ * TERMS AND CONDITIONS:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 public class RunApp {
 	
 	public static void main(String[] args) throws Exception {
 		ReadPDF pdf = new ReadPDF();
-		View view = new View(pdf);
+		
+		/**Notifiy if Update is available **/
+		UpdateNotifier un = new UpdateNotifier();
+		
+		if(un.IsNewVersionAvailable()) {
+			/** Load The Update View **/
+			UpdateView av = new UpdateView(pdf);
+		}
+		else {
+			/** Load The App View **/
+			View view = new View(pdf);
+		}	
 	}
 
 }
--- src/model/ReadPDF.java
+++ src/model/ReadPDF.java
@@ -5,7 +5,31 @@ import java.util.Vector;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
-
+/**
+ * 
+ * ReadPDF.java
+ * 
+ * 
+ * @author Fadi Asbih
+ * @email fadi_asbih@yahoo.de
+ * @version 1.1.0  04/02/2012
+ * @copyright 2012
+ * 
+ * TERMS AND CONDITIONS:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 public class ReadPDF {
 
 	Vector<String> courses = new Vector();
@@ -51,66 +75,63 @@ public class ReadPDF {
 		double sumCredit = 0;
 		double s=0;
 		double s1=0;
-		double s2=0;
 		double sumcredit = 0;
-		int fach=0;
-		int fs=0;
+		int rankedSubjects=0;
+		int subjects=0;
 		String x = textStripper.getText(pddDocument);
 		String[] lines = getLines(x);
+		
+		// find all passed rankedSubjects and save them in a Vector.
 		for(int i=0; i<lines.length; i++) {
-			if(lines[i].contains("BE") && lines[i].contains(".")) {
+			if(lines[i].contains("BE")) {
 				courses.addElement(lines[i]);
 			}
 		}
 		
 		for(int i=0; i<courses.size(); i++) {
-			int sn = courses.elementAt(i).lastIndexOf(".")+6;
-			int en = courses.elementAt(i).lastIndexOf(".")+9;
 			
-//			System.out.println(s+" "+e);
-			String note = courses.elementAt(i).substring(sn,en);
-			String nott = note.replace(',', '.');
-//			System.out.println(courses.elementAt(i).indexOf("PL") > -1);
-			if(!note.contains("BE") && (courses.elementAt(i).indexOf("PL") > -1 || courses.elementAt(i).indexOf("GL") > -1 || courses.elementAt(i).indexOf("WL") > -1)) {
-				fach++;
-				fs++;
-				int sc = courses.elementAt(i).lastIndexOf(".")+13;
-				int ec = courses.elementAt(i).lastIndexOf(".")+15;
-				String credit = courses.elementAt(i).substring(sc,ec);
-
-				not = Double.parseDouble(nott);
-				sumCredit = Double.parseDouble(credit);
+			// first Check if the line is an Exam.
+			if(isExam(courses, i)) {
 				
-				s1 = s1 + not*sumCredit;
-				s = s + sumCredit;
+				// second Check if the Exam is rated.
+				if(isRated(courses, i)) {
+					
+					// Get the mark of the Subject.
+					String mark = getMark(courses, i);
+					// Count the number of the rankedSubjects.
+					rankedSubjects++;
+					// Count the number of the Subjects.
+					subjects++;
+					// Get the Credit Points of the Subject.
+					String credit = getCredit(courses, i);
+					
+					not = Double.parseDouble(mark);
+					sumCredit = Double.parseDouble(credit);
 				
-				sumcredit = sumcredit + sumCredit;
-//				
-//				System.out.println(not);
-//				System.out.println(credit);
-//				System.out.println(courses.elementAt(i));
-			}
-			else if(courses.elementAt(i).indexOf("PL") > -1 || courses.elementAt(i).indexOf("GL") > -1 || courses.elementAt(i).indexOf("WL") > -1){
-				fs++;
-				int sc = courses.elementAt(i).lastIndexOf(".")+9;
-				int ec = courses.elementAt(i).lastIndexOf(".")+11;
-				String credit = courses.elementAt(i).substring(sc,ec);
-				sumCredit = Double.parseDouble(credit);
-				sumcredit = sumcredit + sumCredit;
-//				System.out.println(credit);
-//				System.out.println(courses.elementAt(i));
+					s1 = s1 + not*sumCredit;
+					s = s + sumCredit;
+					sumcredit = sumcredit + sumCredit;
+					
+					// Debug
+					System.out.println("Note: "+mark);
+					System.out.println("Credit: " + credit);
+					System.out.println(courses.elementAt(i));
+				}
+				else { // Not Rated Exams.
+					subjects++;
+					String credit = getCredit(courses, i);
+					sumCredit = Double.parseDouble(credit);
+					sumcredit = sumcredit + sumCredit;
+				}
 			}
 		}
+		
+		
 		setEndMark(s1/s+"");
 		setCredits((int)sumcredit+"");
-		setSubjects(fs+"");
-		setSubjectsWithNote(fach+"");
-//		System.out.println((int)sumcredit);
-		pddDocument.close();
-		
-//		System.out.println(s1/s);
-		
-		
+		setSubjects(subjects+"");
+		setSubjectsWithNote(rankedSubjects+"");
+		pddDocument.close();	
 	}
 	
 	public String[] getLines(String input) {
@@ -124,6 +145,44 @@ public class ReadPDF {
 
 	public void setEndMark(String endMark) {
 		this.endMark = endMark;
+	}
+	
+	public boolean isExam(Vector<String> vector, int index) {
+		if(vector.elementAt(index).indexOf("PL") > -1)
+			return true;
+		if(vector.elementAt(index).indexOf("SL") > -1)
+			return true;
+		if(vector.elementAt(index).indexOf("WL") > -1)
+			return true;
+		if(vector.elementAt(index).indexOf("PR") > -1)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isRated(Vector<String> vector, int index) {
+		if(vector.elementAt(index).contains(","))
+			return true;
+		else
+			return false;
+	}
+	
+	public String getCredit(Vector<String> vector, int index) {
+		int startCreditPosition = vector.elementAt(index).lastIndexOf("BE")+3;
+		int endCreditPosition = vector.elementAt(index).lastIndexOf("BE")+5;
+		String credit = vector.elementAt(index).substring(startCreditPosition,endCreditPosition);
+		
+		return credit;
+	}
+	
+	public String getMark(Vector<String> vector, int index) {
+		// find the start & end mark position of the Exam.
+		int startMarkPosition = vector.elementAt(index).lastIndexOf("B")-4;
+		int endMarkPosition = vector.elementAt(index).lastIndexOf("B")-1;
+
+		// Replace the Comma with a dot in order to be able to calculate.
+		String mark = vector.elementAt(index).substring(startMarkPosition,endMarkPosition).replace(',', '.');
+		return mark;
 	}
 	
 }
